4775bf2cd980a724f5a44c28eaf0511426038c11
AidenRWise <aiwise@ucsd.edu>
2022-11-17 15:19:49 -0800

    changes

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,53 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList; 
+
+class greaterThan3 implements StringChecker{
+
+	public boolean checkString(String s){
+
+		if(s.length()>3){
+			  return true;
+		}
+		return false;
+
+	}
+
+}
+
 
 public class TestListExamples {
   // Write your grading tests here!
+
+
+
+@Test
+public void TestFilter(){
+
+ArrayList<String> input = new ArrayList<>();
+input.add("app");input.add("apple2");input.add("apple3");
+ArrayList<String> answer = new ArrayList<>();
+answer.add("apple2");answer.add("apple3");
+
+StringChecker sc = new greaterThan3();
+
+assertEquals(answer,ListExamples.filter(input,sc));
+
+}
+
+@Test
+public void testMerge(){
+
+	ArrayList<String> input = new ArrayList<>();
+	input.add("apple");input.add("banana");input.add("flamingo");
+	ArrayList<String> input2 = new ArrayList<>();
+	input2.add("az");input2.add("coconut");input2.add("zebra");
+	ArrayList<String> answer = new ArrayList<>();
+	answer.add("apple");answer.add("az");answer.add("banana");answer.add("coconut");answer.add("flamingo");answer.add("zebra");
+
+	assertEquals(answer,ListExamples.merge(input,input2));
+
+}
+
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,47 @@
 # Create your grading script here
 
-set -e
-
 rm -rf student-submission
+echo "removed everything from student-submission"
+
+
 git clone $1 student-submission
+
+cp -r lib student-submission
+
+cp TestListExamples.java student-submission
+echo "copied the tests to student-submission"
+
+cd student-submission
+echo "changed directory to student-submission"
+
+FILE=ListExamples.java
+
+if [ ! -f "$FILE" ];
+then
+	echo "correct file not on system: fail 0/2"
+	exit
+
+fi
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java 2> compileError.txt
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > goodOutput.txt
+
+echo "after the javac and java"
+
+if [ -s compileError.txt ]
+then
+echo "compile Error : Fail 0/2"
+exit
+fi
+
+
+if grep -q "2 tests" "goodOutput.txt";
+then 
+echo "You passed 2/2"
+exit
+fi
+
+cat goodOutput.txt
+echo "You didnt pass all the test, you failed 0/2"
+
 
